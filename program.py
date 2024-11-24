18521aa12d96e6c8a7c223108a5b6d94bcc242b2
vinodkp007 <vinodprasad248@gmail.com>
2024-11-24 12:36:21 +0530

    Made a changes in this code

--- program.py
+++ program.py
@@ -1,4 +1,6 @@
+# with all six stone i could simply snap my finger you would all cease to exist i call that mercy.
 a = int(input("Enter a Number: "))
 b = int(input("Enter a Number: "))
 c = a + b
 print(c)
+print("Making a changes in this code")
