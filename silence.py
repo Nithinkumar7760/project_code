89cca8e069ece5d68f20c1cf9f0fa9aefc08dd05
vinodkp007 <vinodprasad248@gmail.com>
2024-11-25 19:07:45 +0530

    Made a simple change

--- silence.py
+++ silence.py
@@ -1,3 +1,5 @@
+# this is a code of a python for the github
 x = int(input("Enter a number: "))
 c = int(input("Enter a number: "))
 print(x + 1, c + 1)
+print("There is only one path to peace the extinction")
